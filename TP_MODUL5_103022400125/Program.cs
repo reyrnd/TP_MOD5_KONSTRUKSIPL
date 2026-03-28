d7e020375c4803dfaeccd94ceb0026a86fcf7b8a
rerxyz_ <reynard16rande@gmail.com>
2026-03-28 18:24:18 +0700

    menambahkan method generic data nim

--- TP_MODUL5_103022400125/Program.cs
+++ TP_MODUL5_103022400125/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void printData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+public class Utama{
+    public static void Main(string[] args)
+    {
+        DataGeneric<string> dataString = new DataGeneric<string>("103022400125");
+        dataString.printData();
+    }
+}
