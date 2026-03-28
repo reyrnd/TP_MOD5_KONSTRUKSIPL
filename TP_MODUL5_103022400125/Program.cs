652561596c813773590278742eb9a34ed2d34c53
rerxyz_ <reynard16rande@gmail.com>
2026-03-28 18:12:46 +0700

    menambahkan method dengan generic

--- TP_MODUL5_103022400125/Program.cs
+++ TP_MODUL5_103022400125/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class HaloGeneric
+{
+    public void SapaUser<T>(T nama)
+    {
+        Console.WriteLine($"Halo user {nama}");
+    }
+}
+
+public class Utama
+{
+    public static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+
+        Console.Write("Masukkan nama Anda: ");
+        string nama = Console.ReadLine();
+
+        halo.SapaUser(nama);
+    }
+}
